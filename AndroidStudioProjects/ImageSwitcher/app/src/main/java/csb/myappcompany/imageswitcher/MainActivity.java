508fcd816422d82aa8a423d7371d11761fc84543
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-04-05 23:09:48 +0530

    imageswitcher

---
+++ AndroidStudioProjects/ImageSwitcher/app/src/main/java/csb/myappcompany/imageswitcher/MainActivity.java
@@ -0,0 +1,20 @@
+package csb.myappcompany.imageswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void switchAv(View view) {
+        Log.i("Info", "Button Pressed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
