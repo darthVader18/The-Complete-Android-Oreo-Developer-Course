e1f2dee7a2fc98904de3487e6d647be8cf58d2ed
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-04-06 13:23:52 +0530

    currencyConverter

--- AndroidStudioProjects/ImageSwitcher/app/src/main/java/csb/myappcompany/imageswitcher/MainActivity.java
+++ AndroidStudioProjects/ImageSwitcher/app/src/main/java/csb/myappcompany/imageswitcher/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void switchAv(View view) {
         Log.i("Info", "Button Pressed");
+
+        ImageView image = (ImageView) findViewById(R.id.dvimageView);
+        image.setImageResource(R.drawable.sauske);
     }
 
     @Override
